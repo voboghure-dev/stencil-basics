a07bc241d42dec9b28c4ccc12b3170714f9fd527
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-11-28 00:57:34 +0600

    Update structure and style of components

--- src/components/my-card/my-card.tsx
+++ src/components/my-card/my-card.tsx
@@ -7,6 +7,37 @@ import { Component, h } from '@stencil/core';
 })
 export class MyCard {
   render() {
-    return <div class="my-card-wrapper">This is my card</div>;
+    let reactContent = (
+      <div>
+        <div class="card-custom" id="react-div">
+          Hello, from React <br />
+          Live Users
+          <button class="btn-react small-btn">Get React Users</button>
+        </div>
+      </div>
+    );
+
+    let stencilContent = (
+      <div>
+        <div class="card-custom" id="stencil-div">
+          Hello, from Stencil
+          <br />
+          Live Users
+          <button class="btn-stencil small-btn">Get Stencil Users</button>
+        </div>
+      </div>
+    );
+
+    let mainContent = (
+      <div class="my-card-wrapper">
+        <button class="btn-stencil">Stencil</button>
+        <button class="btn-react">React</button>
+
+        {reactContent}
+        {stencilContent}
+      </div>
+    );
+
+    return mainContent;
   }
 }
