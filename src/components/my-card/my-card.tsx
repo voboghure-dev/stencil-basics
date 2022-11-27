5ff470a5d45856ccf78870e71d54518e5bf604c5
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-11-28 01:28:38 +0600

    Use Prop

--- src/components/my-card/my-card.tsx
+++ src/components/my-card/my-card.tsx
@@ -1,4 +1,4 @@
-import { Component, h } from '@stencil/core';
+import { Component, h, Prop } from '@stencil/core';
 
 @Component({
   tag: 'my-card',
@@ -6,13 +6,21 @@ import { Component, h } from '@stencil/core';
   shadow: true,
 })
 export class MyCard {
+  @Prop({ mutable: true }) userName: string;
+
+  changeName() {
+    this.userName = 'Name Changed!';
+  }
+
   render() {
     let reactContent = (
       <div>
         <div class="card-custom" id="react-div">
           Hello, from React <br />
           Live Users
-          <button class="btn-react small-btn">Get React Users</button>
+          <button class="btn-react small-btn" onClick={this.changeName.bind(this)}>
+            Get React Users
+          </button>
         </div>
       </div>
     );
@@ -30,6 +38,7 @@ export class MyCard {
 
     let mainContent = (
       <div class="my-card-wrapper">
+        <h1>Hi, I am {this.userName}</h1>
         <button class="btn-stencil">Stencil</button>
         <button class="btn-react">React</button>
 
